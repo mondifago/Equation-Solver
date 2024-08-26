d77bb7b91e8bd293e9fae80079962b556eee71c9
ugochukwumaduakor <umaduakor@hotmail.com>
2024-08-27 04:13:33 +1000

    start project with quadratic eqn

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace EquationSolver;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a;
+        int b;
+        int c;
+        //int y = Convert.ToInt32(Console.ReadLine());
+        //int z = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("..........................Welcome to Equation Solver..........................");
+        Console.WriteLine("This Program solves different basic mathematics equations");
+        Console.WriteLine("For Quadratic Equation: ax² + bx + c = 0");
+        Console.WriteLine("Please insert the values of a, b, and c");
+        Console.Write("a = ");
+        a = Convert.ToInt32(Console.ReadLine());
+        Console.Write("b = ");
+        b = Convert.ToInt32(Console.ReadLine());
+        Console.Write("c = ");
+        c = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"the equation to solve is {a}x² + {b}x + {c} = 0");
+        //Console.WriteLine($"solution is (x + {y})(x + {z})");
+        //if (b == (y + z) && y * z == a * c)
+        {
+            
+                
+        }
+
+    }
+} 
