6d2cb4157292a361ba4d09de0fc31a07e163039a
Ugochukwu. N Maduakor <umaduakor@hotmail.com>
2026-04-01 02:07:52 +1100

    fix #13 Refactor EquationUI with class models

--- EquationUI.cs
+++ EquationUI.cs
@@ -30,9 +30,9 @@
             Console.WriteLine("In a Quadratic Equation, a cannot be zero");
         }
 
-        public static void ShowEquation(double a, double b, double c)
+        public static void ShowEquation(QuadraticEquation equation)
         {
-            Console.WriteLine($"The equation to solve is {a}x² + {b}x + {c} = 0");
+            Console.WriteLine($"The equation to solve is {equation.A}x² + {equation.B}x + {equation.C} = 0");
         }
 
         public static void ShowNoRealSolutions()
@@ -40,10 +40,10 @@
             Console.WriteLine("No real solutions.");
         }
 
-        public static void ShowSolutions(double x1, double x2)
+        public static void ShowSolutions(QuadraticEquationSolution solution)
         {
-            Console.WriteLine($"x = {x1} or x = {x2}");
-            Console.WriteLine($"Factorised form: (x - {x1})(x - {x2}) = 0");
+            Console.WriteLine($"x = {solution.X1} or x = {solution.X2}");
+            Console.WriteLine($"Factorised form: (x - {solution.X1})(x - {solution.X2}) = 0");
         }
 
         public static void ShowExitMessage()
