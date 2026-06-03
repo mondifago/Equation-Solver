92661065d00220efcd47a26ec8eb6d4f4b52fedc
Ugochukwu. N Maduakor <umaduakor@hotmail.com>
2026-06-03 12:50:37 +1000

    fix #31 Add Input Validation test

---
+++ EquationSolver.Tests/ValidationTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EquationSolver.Tests
+{
+    public class ValidationTests
+    {
+        [Theory]
+        [InlineData("3", true)]
+        [InlineData("3.14", true)]
+        [InlineData("-5", true)]
+        [InlineData("0", true)]
+        [InlineData("", false)]
+        [InlineData("   ", false)]
+        [InlineData("abc", false)]
+        [InlineData("12abc", false)]
+        [InlineData(null, false)]
+        public void IsValidNumber_ReturnsExpected(string input, bool expected)
+        {
+            Assert.Equal(expected, EquationLogic.IsValidNumber(input));
+        }
+    }
+}
